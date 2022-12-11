e6ca6e7efbf50971c92cc332dad313e38a12195a
zct <317712914@qq.com>
2022-12-11 21:50:50 +0800

    add 122

---
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=122 lang=cpp
+ *
+ * [122] Best Time to Buy and Sell Stock II
+ */
+
+// @lc code=start
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit = 0;
+
+        for(int i = 1; i < prices.size(); i++){
+            if(prices[i] > prices[i-1]){
+                profit += (prices[i] - prices[i-1]);
+            }
+        }
+        return profit;
+        
+    }
+};
+// @lc code=end
+
