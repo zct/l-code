bdc8029215c91bdf165bd30b062b1f8276dfb6f3
zct <317712914@qq.com>
2023-01-03 20:28:41 +0800

    half 143

---
+++ 143.reorder-list.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode id=143 lang=cpp
+ *
+ * [143] Reorder List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    void reorderList(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        //
+        while(fast->next != nullptr && fast->next->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* halfR = reverse(slow->next);
+        slow->next = nullptr;
+        while(head != nullptr && halfR != nullptr){
+            ListNode* aNext = head->next;
+            ListNode* bNext = halfR->next;
+            head->next = halfR;
+            halfR->next = aNext;
+            head = aNext;
+            halfR = bNext;
+        }
+        return;
+    }
+
+    ListNode* reverse(ListNode* head){
+        if(head == nullptr){
+            return nullptr;
+        }
+        ListNode* prev = nullptr;
+        ListNode *curr = head;
+        ListNode *next = curr->next;
+        while(next != nullptr){
+            prev = curr;
+            ListNode *temp = next;
+            next = next->next;
+            curr->next->next = prev;
+            curr = temp;
+        }
+        curr->next = prev;
+        return curr;
+    }
+};
+// @lc code=end
+
