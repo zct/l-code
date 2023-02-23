0568e7b6d20fcd9a6e420aef5d269d2051008a8f
zct <317712914@qq.com>
2023-02-23 19:59:14 +0800

    add 673

--- 673.number-of-longest-increasing-subsequence.cpp
+++ 673.number-of-longest-increasing-subsequence.cpp
@@ -10,21 +10,27 @@ using namespace std;
 
 class Solution {
 public:
-    //dp
+    //需要保存两个状态
     int findNumberOfLIS(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n, 1);
-        int longNum = 1;
+        vector<vector<int> > dp(n, vector<int>(2, 1));
+        set<int> longIndex;
         int curLen = 1;
         for(int i = 1; i< n; i++){
             for(int j = 0; j < i; j++){
                 if(nums[i] > nums[j]){
-                    dp[i] = max(dp[j]+1, dp[i]);
-                    if(dp[i] > curLen){
-                        longNum = 1;
-                        curLen = dp[i];
-                    } else if(dp[i] == curLen){
-                        longNum++;
+                    if(dp[j][0] + 1 > dp[i][0]){
+                        dp[i][0] = dp[j][0] + 1;
+                        dp[i][1] = dp[j][1];
+                    } else if(dp[j][0] +1 == dp[i][0]){
+                        dp[i][1] += dp[j][1];
+                    }
+                    if(dp[i][0] > curLen){
+                        curLen = dp[i][0];
+                        longIndex.clear();
+                        longIndex.insert(i);
+                    } else if(dp[i][0] == curLen){
+                        longIndex.insert(i);
                     }
                 }
             }
@@ -32,7 +38,11 @@ public:
         if(curLen == 1){
             return n;
         }
-        return longNum;
+        int res = 0;
+        for(auto i:longIndex){
+            res += dp[i][1];
+        }
+        return res;
     }
 };
 // @lc code=end
