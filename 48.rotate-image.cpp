ecdce8e6fd537632472ae3786cc8070ec60c0b2b
zct <317712914@qq.com>
2023-03-15 15:01:18 +0800

    add 48

---
+++ 48.rotate-image.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=48 lang=cpp
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        // i,j -> j, n-i-1 -> n-i-1, n-j-1 -> n-j-1, i
+        int n = matrix.size();
+        for(int i = 0; i < n/2; i++){
+            for(int j = i; j < n - i -1 ; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[n-j-1][i];
+                matrix[n-j-1][i] = matrix[n-i-1][n-j-1];
+                matrix[n-i-1][n-j-1] = matrix[j][n-i-1];
+                matrix[j][n-i-1] = temp;
+            }
+        }
+    }
+};
+// @lc code=end
+
