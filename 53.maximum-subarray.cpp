e5d5a3e40e1dd6017d426c38243b7ab046394fa0
zct <317712914@qq.com>
2023-11-29 17:02:04 +0800

    53 dp

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -10,7 +10,22 @@ using namespace std;
 
 class Solution {
 public:
+    //dp[i] = max(dp[i-1]+nums[i], nums[i])
+    //return max(dp[0..i]);
     int maxSubArray(vector<int>& nums) {
+        if(nums.size() == 0){
+            return 0;
+        }
+        vector<int> dp(nums.size(), 0);
+        dp[0] = nums[0];
+        int ans = dp[0];
+        for(int i = 1; i < nums.size(); i++){
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            ans = max(dp[i], ans);
+        }
+        return ans;
+    }
+    int maxSubArray2(vector<int>& nums) {
         int res = 0;
         int sum = 0;
         for(int i = 0; i < nums.size(); i++){
