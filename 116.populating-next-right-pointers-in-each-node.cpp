f0a408a2d76a4316b5cf6fb600ee12f08b455b0d
zct <317712914@qq.com>
2023-11-30 10:34:49 +0800

    add 116

---
+++ 116.populating-next-right-pointers-in-each-node.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=116 lang=cpp
+ *
+ * [116] Populating Next Right Pointers in Each Node
+ */
+
+// @lc code=start
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* left;
+    Node* right;
+    Node* next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node* _left, Node* _right, Node* _next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    //bfs
+    Node* connect(Node* root) {
+        if(root == nullptr){
+            return nullptr;
+        }
+        queue<Node*> nodeQueue;
+        nodeQueue.push(root);
+        while(nodeQueue.size() != 0){
+            int size = nodeQueue.size();
+            for(int i = 0; i < size; i++){
+                Node* prev = nodeQueue.front();
+                if(prev->left != nullptr){
+                    nodeQueue.push(prev->left);
+                }
+                if(prev->right != nullptr){
+                    nodeQueue.push(prev->right);
+                }
+                nodeQueue.pop();
+                if(i != size - 1){
+                    Node* next = nodeQueue.front();
+                    prev->next = next;
+                } else {
+                prev->next = nullptr;
+                }
+            }
+        }
+        return root;
+    }
+};
+// @lc code=end
+
