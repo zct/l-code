3d0efc4968e78be809d47f5861fae9156568e9b8
zct <317712914@qq.com>
2024-01-13 21:42:17 +0800

    update 122

--- 122.best-time-to-buy-and-sell-stock-ii.cpp
+++ 122.best-time-to-buy-and-sell-stock-ii.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit2(vector<int>& prices) {
         int profit = 0;
 
         for(int i = 1; i < prices.size(); i++){
@@ -22,6 +22,28 @@ public:
         return profit;
         
     }
+
+    //动态规划的思路
+    //0 不持有
+    //1 持有
+
+    //dp[i][0] = max(dp[i-1][1] + prices[i], dp[i-1][0])
+    //dp[i][1] = max(dp[i-1][0] - prices[i], dp[i-1][1])
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n <= 0){
+            return 0;
+        }
+        vector<vector<int> > dp(n , vector<int>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] = -1 * prices[0];
+        for(int i = 1; i < n; i++ ){
+            dp[i][0] = max(dp[i-1][1] + prices[i], dp[i-1][0]);
+            dp[i][1] = max(dp[i-1][0] - prices[i], dp[i-1][1]);
+        }
+        return dp[n-1][0];
+    }
+
 };
 // @lc code=end
 
