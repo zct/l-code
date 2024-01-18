895827dfbb0c4130d9388b6f95723d34680c642a
zct <317712914@qq.com>
2024-01-18 12:39:43 +0800

    update 55

--- 55.jump-game.cpp
+++ 55.jump-game.cpp
@@ -11,7 +11,9 @@ using namespace std;
 
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+    //这个动态规划不是最优解
+    //考虑贪心算法
+    bool canJumpa2(vector<int>& nums) {
         vector<bool> f(nums.size());
         for(auto iter = f.begin(); iter != f.end(); iter++){
             *iter = false;
@@ -28,6 +30,21 @@ public:
             }
         }
         return f[nums.size() -1];
+    }
+    //更新最远可到达的位置
+    //如果大于，则更新
+    bool canJump(vector<int>& nums) {
+        bool res = false;
+        int maxIndex = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i <= maxIndex){
+                maxIndex = max(maxIndex, i+nums[i]);
+                if(maxIndex >= nums.size()-1){
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 };
 // @lc code=end
